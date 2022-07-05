c063141f6ec9e1213e12fc00fec4bd47df3ca27b
wangsp14 <wangsp14@midea.com>
2022-07-05 23:28:09 +0800

    element-plus 组件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,87 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { MoreFilled } from '@element-plus/icons-vue'
+import { ElMessageBox,ElNotification } from 'element-plus'
 
+const open1 = () => {
+  ElNotification({
+    title: 'Success',
+    message: 'This is a success message',
+    type: 'success',
+  })
+}
 defineProps<{ msg: string }>()
-
+const activities = [
+  {
+    content: 'Event start',
+    timestamp: '2018-04-15',
+  },
+  {
+    content: 'Approved',
+    timestamp: '2018-04-13',
+  },
+  {
+    content: 'Success',
+    timestamp: '2018-04-11',
+  },
+]
 const count = ref(0)
+const dialogVisible = ref(false)
+
+const handleClose = (done: () => void) => {
+  ElMessageBox.confirm('Are you sure to close this dialog?')
+          .then(() => {
+            done()
+          })
+          .catch(() => {
+            // catch error
+          })
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-  <el-button>I am ElButton</el-button>
+  <el-button plain @click="open1"> Success </el-button>
+  <el-button text @click="dialogVisible = true"
+  >click to open the Dialog</el-button
+  >
+
+  <el-dialog
+          v-model="dialogVisible"
+          title="Tips"
+          width="30%"
+          :before-close="handleClose"
+  >
+    <span>This is a message</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+        >Confirm</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
+  <el-timeline>
+    <el-timeline-item timestamp="2018/4/12" placement="top">
+      <el-card>
+        <h4>Update Github template</h4>
+        <p>Tom committed 2018/4/12 20:46</p>
+      </el-card>
+    </el-timeline-item>
+    <el-timeline-item timestamp="2018/4/3" placement="top">
+      <el-card>
+        <h4>Update Github template</h4>
+        <p>Tom committed 2018/4/3 20:46</p>
+      </el-card>
+    </el-timeline-item>
+    <el-timeline-item timestamp="2018/4/2" placement="top">
+      <el-card>
+        <h4>Update Github template</h4>
+        <p>Tom committed 2018/4/2 20:46</p>
+      </el-card>
+    </el-timeline-item>
+  </el-timeline>
   <p>
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
